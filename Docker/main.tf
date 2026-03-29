bf7c2815b3b3e12e6682757ac715dcb9cd857353
MuddyThunder1040 <vishnupardha1040@gmail.com>
2026-03-29 00:18:02 -0700

    V002 Add initial Docker configuration with nginx container

--- Docker/main.tf
+++ Docker/main.tf
@@ -17,7 +17,7 @@ resource "docker_image" "nginx" {
 
 resource "docker_container" "nginx_container" {
   name  = "nginx"
-  image = docker_image.nginx
+  image = docker_image.nginx.name
   ports {
     internal = 80
     external = 8080
