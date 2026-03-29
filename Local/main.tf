7391b7f4e66fd1a272f2d11244f3c11596e7c4fe
Vishnu3259 <vishnupardha3259@gmail.com>
2026-03-29 13:21:10 -0700

    Add S3 backend configuration to main Terraform file and create new VPC main.tf

--- Local/main.tf
+++ Local/main.tf
@@ -10,6 +10,17 @@ terraform {
     }
   }
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "terraform-backup-tank"
+    key            = "dev/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
+
 provider "local" {
   
 }
