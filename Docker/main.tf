912a63e2e294917b9c93ac938e54faca6ff3f23b
Vishnu3259 <vishnupardha3259@gmail.com>
2026-04-04 00:42:06 -0700

    INFR-4 Added terraform cloud to store tfstate

--- Docker/main.tf
+++ Docker/main.tf
@@ -5,26 +5,29 @@ terraform {
       version = "~> 3.0"
     }
   }
+    cloud {
+    organization = "oiasis-org"
+
+    workspaces {
+      name = "homelab-tf"
+    }
+  }
 }
 
 provider "docker" {}
 
-# ✅ Network
 resource "docker_network" "cassandra_network" {
   name = var.cas_network
 }
 
-# ✅ Image
 resource "docker_image" "cassandra_image" {
   name = "cassandra:latest"
 }
 
-# ✅ Volume (data persistence)
 resource "docker_volume" "cassandra_data" {
   name = "cassandra_data_seed"
 }
 
-# ✅ Seed Node
 resource "docker_container" "cassandra_seed_node" {
   name  = "cassandra-seed-node"
   image = docker_image.cassandra_image.image_id
@@ -33,18 +36,15 @@ resource "docker_container" "cassandra_seed_node" {
     name = docker_network.cassandra_network.name
   }
 
-  # 🔥 Resource limits
   cpu_shares = 512
   memory     = 2048
   memory_swap = 2048
 
-  # 💾 Persistent storage
   volumes {
     volume_name    = docker_volume.cassandra_data.name
     container_path = "/var/lib/cassandra"
   }
 
-  # 🧠 Cassandra config
   env = [
     "CASSANDRA_CLUSTER_NAME=MyCassandraCluster",
     "CASSANDRA_SEEDS=cassandra-seed-node",
@@ -52,13 +52,11 @@ resource "docker_container" "cassandra_seed_node" {
     "HEAP_NEWSIZE=256M"
   ]
 
-  # 🌐 Expose ONLY CQL port
   ports {
     internal = 9042
     external = 9042
   }
 
-  # 🔧 Required for Cassandra stability
   ulimit {
     name = "nofile"
     soft = 100000
