68bdbb66802bba69626d3602e280d6b1524966cb
Vishnu3259 <vishnupardha3259@gmail.com>
2026-04-04 09:02:31 -0700

    INFR-4 Refactored docker

--- Docker/main.tf
+++ Docker/main.tf
@@ -5,7 +5,7 @@ terraform {
       version = "~> 3.0"
     }
   }
-    cloud {
+      cloud {
     organization = "oiasis-org"
 
     workspaces {
@@ -16,17 +16,33 @@ terraform {
 
 provider "docker" {}
 
+
+variable "cas_network" {
+  default = "cassandra_network"
+}
+
+
 resource "docker_network" "cassandra_network" {
   name = var.cas_network
 }
+
 
 resource "docker_image" "cassandra_image" {
   name = "cassandra:latest"
 }
 
-resource "docker_volume" "cassandra_data" {
-  name = "cassandra_data_seed"
+
+# Seed node volume
+resource "docker_volume" "cassandra_seed_data" {
+  name = "cassandra-data-seed"
 }
+
+# Worker node volumes
+resource "docker_volume" "cassandra_non_seed_data" {
+  count = var.non_seed_node_count
+  name  = "cassandra-data-non-seed-${count.index}"
+}
+
 
 resource "docker_container" "cassandra_seed_node" {
   name  = "cassandra-seed-node"
@@ -36,15 +52,18 @@ resource "docker_container" "cassandra_seed_node" {
     name = docker_network.cassandra_network.name
   }
 
-  cpu_shares = 512
-  memory     = 2048
+  # 🔥 Resource limits
+  cpu_shares  = 512
+  memory      = 2048
   memory_swap = 2048
 
+  # 💾 Persistent storage
   volumes {
-    volume_name    = docker_volume.cassandra_data.name
+    volume_name    = docker_volume.cassandra_seed_data.name
     container_path = "/var/lib/cassandra"
   }
 
+  # 🧠 Cassandra config
   env = [
     "CASSANDRA_CLUSTER_NAME=MyCassandraCluster",
     "CASSANDRA_SEEDS=cassandra-seed-node",
@@ -52,14 +71,65 @@ resource "docker_container" "cassandra_seed_node" {
     "HEAP_NEWSIZE=256M"
   ]
 
+  # 🌐 External access (only seed)
   ports {
     internal = 9042
     external = 9042
   }
 
+  # 🔧 Required for Cassandra
   ulimit {
     name = "nofile"
     soft = 100000
     hard = 100000
   }
+}
+
+
+resource "docker_container" "cassandra_nodes" {
+  count = var.non_seed_node_count
+
+  name  = "cassandra-node-${count.index}"
+  image = docker_image.cassandra_image.image_id
+
+  networks_advanced {
+    name = docker_network.cassandra_network.name
+  }
+
+  # 🔥 Resource limits
+  cpu_shares  = 512
+  memory      = 2048
+  memory_swap = 2048
+
+  # 💾 Unique volume per node
+  volumes {
+    volume_name    = docker_volume.cassandra_non_seed_data[count.index].name
+    container_path = "/var/lib/cassandra"
+  }
+
+  # 🧠 Cassandra config
+  env = [
+    "CASSANDRA_CLUSTER_NAME=MyCassandraCluster",
+    "CASSANDRA_SEEDS=cassandra-seed-node",
+    "MAX_HEAP_SIZE=1G",
+    "HEAP_NEWSIZE=256M"
+  ]
+
+  # 🌐 Unique ports per node
+  ports {
+    internal = 9042
+    external = 9043 + count.index
+  }
+
+  # 🔧 Required for Cassandra
+  ulimit {
+    name = "nofile"
+    soft = 100000
+    hard = 100000
+  }
+
+  # ⏳ Ensure seed starts first
+  depends_on = [
+    docker_container.cassandra_seed_node
+  ]
 }
--- Docker/variables.tf
+++ Docker/variables.tf
@@ -3,3 +3,13 @@ variable "cas_network" {
     type = string
     description = "The name of the Docker network for Cassandra containers"
 }
+variable "seed_node_cont" {
+    default = 1
+    type = number
+    description = "The number of seed nodes to create for the Cassandra cluster"
+}
+variable "non_seed_node_count" {
+    default = 3
+    type = number
+    description = "The number of non-seed nodes to create for the Cassandra cluster"
+}
