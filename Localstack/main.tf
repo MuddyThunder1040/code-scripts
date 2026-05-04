8f0162257a1d8500204e352a6655fac5adf10ff5
MuddyThunder1040 <kvp1040@gmail.com>
2026-05-03 17:12:25 -0700

    INFR-5 Added the Localstack to tf operations | Testing access for s3

--- Localstack/main.tf
+++ Localstack/main.tf
@@ -6,11 +6,30 @@ provider "aws" {
   s3_use_path_style = true
 
   endpoints {
+    dynamodb = "http://localhost:4566"
     s3 = "http://localhost:4566"
     sts = "http://localhost:4566"
+  }
+  cloud {
+    organization = "oiasis-org"
+
+    workspaces {
+      name = "homelab-tf"
+    }
   }
 
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
+}
+
+resource "aws_dynamodb_table" "localstack_table" {
+  name         = "localstack-table"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
 }
